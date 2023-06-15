a236a49a93e8febc162e95f23d3adeaa859f909d
pratiksha <maykulkarpratiksha@gmail.com>
2023-06-15 12:09:27 +0530

    files added

--- src/index.js
+++ src/index.js
@@ -6,27 +6,27 @@ import './index.css';
 const books = [
   {
    title: 'Wings Of Fire',
-   imageUrl: '/images/Wings of fire.jpg',
+   imageUrl: require('./images/Wings of fire.jpg'),
    link: 'https://www.amazon.in/Wings-Fire-Autobiography-Abdul-Kalam/dp/8173711461'
   },
   {
     title: 'Turning Points',
-    imageUrl: '/images/Turning points.jpg',
+    imageUrl:  require('./images/Turning points.jpg'),
     link: 'https://www.amazon.in/Turning-Points-Journey-Challanges-Challenges/dp/9350293471'
   },
   {
     title: 'Story Of Experiments With Truth',
-    imageUrl: '/images/Story of Experiments with truth.jpg',
+    imageUrl: require('./images/Story of Experiments with truth.jpg'),
     link: 'https://www.amazon.in/Mahatma-Gandhi-Autobiography-Story-Experiments/dp/8172343116'
   },
   {
     title: 'Ignited Minds',
-    imageUrl: '/images/Ignited Minds.jpg',
+    imageUrl: require('./images/Ignited Minds.jpg'),
     link: 'https://www.amazon.in/Ignited-Minds-Unleashing-power-within/dp/0143424122'
   },
   {
     title: 'Forge Your Future',
-    imageUrl: '/images/forge your future1.webp',
+    imageUrl: require('./images/forge your future1.webp'),
     link: 'https://www.amazon.in/Forge-Future-P-J-Abdul-Kalam/dp/9350642794'
   }
 ];
